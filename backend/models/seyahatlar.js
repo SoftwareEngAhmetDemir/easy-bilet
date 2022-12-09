fef91ecc22d3f0ad7ad7d8102a768c6dd6359520
ademir <ademir@promaks.com.tr>
2022-12-09 23:15:15 +0300

    add reserve api

--- backend/models/seyahatlar.js
+++ backend/models/seyahatlar.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
-
+const arrayUniquePlugin = require('mongoose-unique-array');
 const { Schema } = mongoose;
 
 const seyahatlar = new Schema({
+    // _id: {type: String},
     otobusFirmasi: { type : String  }, // String is shorthand for {type: String}
     kalkisSaati: { type : String  },
     fromTo:  { type : String },
     Ucret: { type : Number  },
+    filled:[String],
+    maxfilled: {type:Number}
 });
+seyahatlar.plugin(arrayUniquePlugin);
 export default seyahatlar;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 import yeniKayit from '../controllers/yenikayit';
 import login from '../controllers/login';
 import seyahatlar from '../controllers/seyahatlar';
+import updateSeyahatlat from '../controllers/update-seyahatlar';
 
 router.use('/yeniKayit', yeniKayit)
 router.use('/login', login)
 router.use('/seyahatlar',seyahatlar)
+router.use('/updateseyahatlar',updateSeyahatlat)
 module.exports = router
